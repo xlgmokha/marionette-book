6dda7d53db9fff74e54da356093507a4152925ce
mo khan <mo@mokhan.ca>
2014-06-26 15:30:32 -0600

    complete chapter 3.

--- src/js/bookstore.js
+++ src/js/bookstore.js
@@ -1,1 +1,101 @@
-var bookStoreApp = new Backbone.Marionette.Application();
+var BookStoreApp = new Backbone.Marionette.Application();
+
+var BookStoreController = Backbone.Marionette.Controller.extend({
+  displayBooks: function() {
+    console.log('display books.');
+  }
+});
+
+var BookStoreRouter = Backbone.Marionette.AppRouter.extend({
+  controller: BookStoreController,
+  appRoutes: {
+    "": "displayBooks"
+  }
+});
+
+BookStoreApp.addInitializer(function() {
+  var controller = new BookStoreController();
+  var router = new BookStoreRouter({controller: controller});
+  console.log('addInitializer');
+});
+
+BookStoreApp.on('initialize:after', function(){
+  if (Backbone.history) {
+    Backbone.history.start();
+  }
+  console.log('message from initialize:after method.');
+});
+BookStoreApp.start();
+
+var BookModel = Backbone.Model.extend({
+  defaults: {
+    id: "1",
+    name: "First",
+  }
+});
+
+var BookCollection = Backbone.Collection.extend({
+  model: BookModel
+});
+
+var bookModel = new BookModel();
+var bookModel2 = new BookModel({id: "2", name: "second"});
+var bookModel3 = new BookModel({id: "3", name: "third"});
+var bookCollection = new BookCollection();
+bookCollection.add(bookModel);
+bookCollection.add(bookModel2);
+bookCollection.add(bookModel3);
+
+var BookListView = Marionette.ItemView.extend({
+  template: '#books-template'
+});
+var view = new BookListView({
+  collection: bookCollection
+});
+view.render();
+
+var CategoryView = Backbone.Marionette.ItemView.extend({
+  tagName: 'li',
+  template: '#categoryTemplate',
+});
+
+var CategoriesView = Backbone.Marionette.CollectionView.extend({
+  tagName: 'ul',
+  className: 'unstyled',
+  itemView: CategoryView
+});
+
+//var categoriesView = new CategoriesView({collection: categories});
+//categoriesView.render();
+
+var CatalogLayout = Backbone.Marionette.LayoutView.extend({
+  template: '#CatalogLayout',
+  regions: {
+    categories: '#categories',
+    products: '#products',
+    order: '#order',
+    book: '#book'
+  }
+});
+
+var catalogLayout = new CatalogLayout();
+catalogLayout.render();
+
+var HandyView = Backbone.Marionette.ItemView.extend({
+  initialize: function(){
+    Backbone.Marionette.ItemView.prototype.initialize.apply(this, arguments);
+  },
+  logMessage: function(message) {
+    console.log(message);
+  }
+});
+
+var BookView = HandyView.extend({
+  alertMessage: function(message) {
+    alert(message);
+  }
+});
+
+var bookView = new BookView();
+bookView.logMessage('hi');
+bookView.alertMessage('bye');
